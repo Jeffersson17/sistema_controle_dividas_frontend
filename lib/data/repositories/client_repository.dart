3b2c60c84d03cc139f0b716e885db08c2ea444fb
Jeffersson <barrosjefferson407@gmail.com>
2025-07-16 11:26:20 -0300

    fix client_repository

--- lib/data/repositories/client_repository.dart
+++ lib/data/repositories/client_repository.dart
@@ -1,11 +1,18 @@
 import 'dart:convert';
 
-import 'package:sistema_controle_dividas_frontend/data/http/exceptions.dart';
 import 'package:sistema_controle_dividas_frontend/data/http/http_client.dart';
 import 'package:sistema_controle_dividas_frontend/data/models/client_model.dart';
 
+import '../http/exceptions.dart';
+import '../models/historical_model.dart';
+
 abstract class IClientRepository {
   Future<List<ClientModel>> getClients();
+  Future<HistoricalModel> updateClientDebt({
+    required String clientId,
+    required double value,
+    required String observation,
+  });
 }
 
 class ClientRepository implements IClientRepository {
@@ -31,6 +38,31 @@ class ClientRepository implements IClientRepository {
       throw NotFoundException(message: 'A url informada não é válida');
     } else {
       throw Exception('Não foi possível carregar os clientes');
+    }
+  }
+
+  @override
+  Future<HistoricalModel> updateClientDebt({
+    required String clientId,
+    required double value,
+    required String observation,
+  }) async {
+    final response = await client.post(
+      url: 'http://10.0.0.175:8000/historical/',
+      body: {
+        'client': clientId,
+        'value': value,
+        'observation': observation,
+      },
+    );
+
+    if (response.statusCode == 201) {
+      final body = jsonDecode(response.body);
+      return HistoricalModel.fromMap(body);
+    } else if (response.statusCode == 400) {
+      throw Exception('Dados inválidos para atualizar dívida.');
+    } else {
+      throw Exception('Erro ao atualizar a dívida do cliente.');
     }
   }
 }
