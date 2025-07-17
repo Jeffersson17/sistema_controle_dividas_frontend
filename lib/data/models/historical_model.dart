c0d21e1f35e13f12cb4ee5b75bf5c4450bb45dc7
Jeffersson Barros <146139830+Jeffersson17@users.noreply.github.com>
2025-07-17 09:23:33 -0300

    Changes to the history page (#8)
    * fix client_page
    
    * historical details
    
    * dependence

--- lib/data/models/historical_model.dart
+++ lib/data/models/historical_model.dart
@@ -1,22 +1,28 @@
 class HistoricalModel {
-  final double value;
+  final int id;
+  final String client; // id do cliente (UUID)
   final String clientName;
+  final double value;
   final String observation;
   final String date;
 
   HistoricalModel({
-    required this.value,
+    required this.id,
+    required this.client,
     required this.clientName,
+    required this.value,
     required this.observation,
     required this.date,
   });
 
   factory HistoricalModel.fromMap(Map<String, dynamic> map) {
     return HistoricalModel(
+      id: map['id'] as int,
+      client: map['client'] as String,
+      clientName: map['client_name'] as String,
       value: double.parse(map['value'].toString()),
-      clientName: map['client_name'],
-      observation: map['observation'],
-      date: map['date'],
+      observation: map['observation'] as String,
+      date: map['date'] as String,
     );
   }
 }
